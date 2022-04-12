0f2bd03c77dce7e7e347a898abbf4fb2650cb7a9
Nhlakanipho Nkokhelo <N.N.TEMBE@outlook.com>
2022-04-12 20:20:46 +0200

    add threads

--- src/com/ikhokha/techcheck/CommentAnalyzer.java
+++ src/com/ikhokha/techcheck/CommentAnalyzer.java
@@ -6,61 +6,57 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
-public class CommentAnalyzer {
-	
-	private File file;
-	public String[] metrics;
-	
-	public CommentAnalyzer(File file) {
-		this.file = file;
-	}
-	
-	public Map<String, Integer> analyze() {
-		
-		Map<String, Integer> resultsMap = new HashMap<>();
-		
-		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-			
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				
-				if (line.length() < 15) {
-					
-					incOccurrence(resultsMap, "SHORTER_THAN_15");
+public class CommentAnalyzer implements Callable<Map<String, Integer>> {
+    private final File file;
+    public List<Metrics>  metricsList;
+    
+    public CommentAnalyzer(File file, List<Metrics> metricsList) {
+        this.file = file;
+        this.metricsList = metricsList;
+    }
 
-				} else if (line.contains("Mover")) {
+    @Override
+    public Map<String, Integer> call() throws Exception {
+        return analyze();
+    }
 
-					incOccurrence(resultsMap, "MOVER_MENTIONS");
-				
-				} else if (line.contains("Shaker")) {
+    public Map<String, Integer> analyze() {
+        var resultsMap = new HashMap<String, Integer>();
+        try (var reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (Metrics metrics : metricsList) {
+                    if(metrics.testLine(line)) {
+                        incOccurrence(resultsMap, metrics.getKey());
+                    }
+                }
+            }
+        } catch (FileNotFoundException  e) {
+            System.out.println("File not found: " + file.getAbsolutePath());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("IO Error processing file: " + file.getAbsolutePath());
+            e.printStackTrace();
+        }
+        return resultsMap;
+    }
 
-					incOccurrence(resultsMap, "SHAKER_MENTIONS");
-				
-				}
-			}
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + file.getAbsolutePath());
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("IO Error processing file: " + file.getAbsolutePath());
-			e.printStackTrace();
-		}
-		
-		return resultsMap;
-		
-	}
-	
-	/**
-	 * This method increments a counter by 1 for a match type on the countMap. Uninitialized keys will be set to 1
-	 * @param countMap the map that keeps track of counts
-	 * @param key the key for the value to increment
-	 */
-	private void incOccurrence(Map<String, Integer> countMap, String key) {
-		countMap.putIfAbsent(key, 0);
-		countMap.put(key, countMap.get(key) + 1);
-	}
+    /**
+     * This method increments a counter by 1 for a match type on the countMap.
+     * Uninitialized keys will be set to 1
+     *
+     * @param countMap the map that keeps track of counts
+     * @param key      the key for the value to increment
+     */
+    private void incOccurrence(Map<String, Integer> countMap, String key) {
+        countMap.putIfAbsent(key, 0);
+        countMap.put(key, countMap.get(key) + 1);
+    }
+
+
 
 }
--- src/com/ikhokha/techcheck/Main.java
+++ src/com/ikhokha/techcheck/Main.java
@@ -1,24 +1,35 @@
 package com.ikhokha.techcheck;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Map<String, Integer> totalResults = new HashMap<>();
-				
-		File commentFolder = new File("docs");
-		File[] commentFiles = commentFolder.listFiles((d, n) -> n.endsWith(".txt"));
-		
-		for (File commentFile : commentFiles) {
-			CommentAnalyzer commentAnalyzer = new CommentAnalyzer(commentFile);
-			Map<String, Integer> fileResults = commentAnalyzer.analyze();
-			addReportResults(fileResults, totalResults);						
+		var totalResults = new HashMap<String, Integer>();
+		var commentFolder = new File("docs");
+		var commentFiles = commentFolder.listFiles((d, n) -> n.endsWith(".txt"));
+		var metricsList = getMetricsList();
+		ExecutorService excecutor = Executors.newFixedThreadPool(commentFiles.length);
+
+		for (var commentFile : commentFiles) {
+			try {
+				Future<Map<String, Integer>> metricsFuture = excecutor.submit(new CommentAnalyzer(commentFile, metricsList));
+				var fileResults = metricsFuture.get();
+				addReportResults(fileResults, totalResults);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}						
 		}
 		
+		excecutor.shutdown();
 		System.out.println("RESULTS\n=======");
 		totalResults.forEach((k,v) -> System.out.println(k + " : " + v));
 	}
@@ -33,11 +44,23 @@ public class Main {
 		for (Map.Entry<String, Integer> entry : fileMap.entrySet()) {
 			var key = entry.getKey();
 			var metricsCount = entry.getValue();
-
 			totalMap.putIfAbsent(key, 0);
 			totalMap.put(key, totalMap.get(key) + metricsCount);
 		}
 		
+	}
+
+	/**
+	 * This method initialize metrics list to used to analyze the comments documents 
+	 */
+	private static List<Metrics> getMetricsList() {
+		var metricsList = new ArrayList<Metrics>();
+		metricsList.add(new Metrics("SHORTER_THAN_15", MetricsType.LESS, 15));
+		metricsList.add(new Metrics("MOVER_MENTIONS", MetricsType.CONTAINS, "Mover"));
+		metricsList.add(new Metrics("SHAKER_MENTIONS", MetricsType.CONTAINS, "Shaker"));
+		metricsList.add(new Metrics("QUESTIONS", MetricsType.CONTAINS, "?"));
+		metricsList.add(new Metrics("SPAM", MetricsType.CONTAINS, "http"));
+		return metricsList;
 	}
 
 }
